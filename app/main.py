6ffbe301c53a92dc44613ae96f76a70bc5c2e152
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2025-12-10 11:34:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.routers import onestation
 
 app = FastAPI()
 
 @app.get("/")
 def health():
     return {"status": "OK", "message": "FastAPI is running on Railway"}
+
+# إضافة ربط الراوتر
+app.include_router(onestation.router)
