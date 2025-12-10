5fd69af107e8c7794a80e67b68e1961519c47e6a
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2025-12-10 11:25:33 +0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Settings:
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+
+settings = Settings()
